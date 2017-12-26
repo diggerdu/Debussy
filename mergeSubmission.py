40f447287eebc9c660dcc09e22972f4364c07160
diggerdu <diggerdu@hotmail.com>
2017-12-26 21:45:58 +0800

    add mergeSubmission.py

--- mergeSubmission.py
+++ mergeSubmission.py
@@ -1,6 +1,4 @@
 import csv
-import soundfile as sf
-import sounddevice as sd
 import argparse
 import time
 import select
@@ -9,6 +7,7 @@ import numpy as np
 from util.getPatch import getLabelDict
 import os
 
+TABLE = 'yes, no, up, down, left, right, on, off, stop, go, silence, unknown'.split(', ')
 parser = argparse.ArgumentParser()
 parser.add_argument("-s", "--submissionFile", required=True, help="submission file")
 parser.add_argument("-p", "--patchPath", help="patch file Path")
@@ -16,20 +15,22 @@ parser.add_argument("-o", "--outputFile", required=True, help="output file")
 args = parser.parse_args()
 
 patchDict = getLabelDict(args.patchPath)
+print(len(patchDict.keys()))
 
 
 with open(args.submissionFile, mode='r') as f:
     reader = csv.reader(f)
     labelDict = {rows[0]:rows[1] for rows in reader if rows[0].endswith('.wav')}
-    
-    assert len(set(labelDict.keys())) == 158538
+    assert len(set(list(labelDict.keys()))) == 158538
 
 mergeDict = dict()
+
 count = 0
 for fn, label in labelDict.items():
     try:
         mergeDict.update({fn:patchDict[fn]})
-        count += 1
+        if patchDict[fn] != label:
+            count += 1
     except:
         mergeDict.update({fn: label})
 
@@ -37,11 +38,13 @@ print("relabel {} entries".format(count))
 
 # check twice
 
-assert len(set(mergeDict.keys())) == 158538
+assert len(set(list(mergeDict.keys()))) == 158538
+for fn, label in mergeDict.items():
+    assert label in TABLE
 
 outputFile = open(args.outputFile, "w+")
 print('fname,label', file=outputFile)
 for fn, label in mergeDict.items():
-    print('{},{}'.format(fn, label), file)
+    print('{},{}'.format(fn, label), file=outputFile)
 outputFile.close()
 
